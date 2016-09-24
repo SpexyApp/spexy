c22a02396f535101e9d34a7be8c5366af6c43918
Utkarsh Jain <utkjain@gmail.com>
2016-09-24 18:59:01 -0400

    Update: Can download Python spexy on client machine

--- UI.java
+++ UI.java
@@ -1,22 +1,28 @@
 package com.combined;
 
+import org.apache.commons.io.FileUtils;
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
 import java.awt.event.ComponentAdapter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.*;
 import java.net.ProtocolException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.StringJoiner;
-//import org.json.*;
-
+import com.sun.deploy.net.HttpResponse;
+import org.apache.commons.httpclient.URI;
+import org.json.*;
 import org.apache.commons.httpclient.*;
+import org.omg.CORBA.Request;
+import sun.misc.IOUtils;
 
 /**
  * Created by Utkarsh on 9/24/2016.
@@ -79,17 +85,44 @@ public class UI extends JFrame
                         sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
                                 + URLEncoder.encode(entry.getValue(), "UTF-8"));
                     byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+                    //String str = sj.toString();
                     int length = out.length;
 
-
-                    //getting from server
-                    http.setFixedLengthStreamingMode(length);
+                    //getting success/fail lead from server
+                    /*http.setFixedLengthStreamingMode(length);
                     http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
                     http.connect();
-                    try(OutputStream os = http.getOutputStream())
-                    {
+                    try(OutputStream os = http.getOutputStream()) {
                         os.write(out);
+                        out.();
                         System.out.println(out);
+                    }
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(http.getInputStream()));
+                    Scanner s = new Scanner(http.getInputStream()).useDelimiter("\\A");
+                    String result = s.hasNext() ? s.next() : ""; **/
+
+                    URL get = new URL("http://nisarg.me:1337/pcauth/" + userid + "/" + userPassword + "/" + usermac + "/" + userpcname);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(get.openStream()));
+                    String inputLine;
+                    while((inputLine = in.readLine()) != null)
+                    {
+                        if(inputLine.equals("false"))
+                        {
+                            JOptionPane.showMessageDialog(null, "There seems to be something wrong with the data you entered. Try again");
+                        }
+                        else
+                        {
+                            URL urlurl = new URL("https://github.com/SpexyApp/spexy/tree/master/spexy");
+                            String path = urlurl.toString();
+                            File file = new File(path);
+                            FileUtils.copyURLToFile(url, file);
+                        }
+                    }
+                    in.close();
+
+                    if(true)
+                    {
+
                     }
                 }
                 catch (UnknownHostException e1)
@@ -130,13 +163,9 @@ public class UI extends JFrame
         UI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         UI.setSize(800, 500);
         UI.setVisible(true);
-
         //Get the mac address from the machine
         //Stores in String sb
         //printStackTrace in case not found
-
-
-
     }
 
 }
