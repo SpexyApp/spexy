1c12fc639c4474386b287f7adfd2da5ae7e61088
nisargkolhe <nisargkolhe@gmail.com>
2016-09-24 18:21:56 -0400

    Completed Dash UI

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -60,7 +60,7 @@
 	app.config(function (ChartJsProvider) {
 	    // Configure all charts
 	    ChartJsProvider.setOptions({
-	      colors: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']
+	      colors : [ '#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']	   
 	    });
 	    // Configure all doughnut charts
 	    ChartJsProvider.setOptions('doughnut', {
@@ -69,18 +69,25 @@
 	    
 	  });
 	  
-	app.controller("LineCtrl", function ($scope) {
+	app.controller("ProcCtrl", function ($scope) {
 
-	  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-	  $scope.series = ['Series A', 'Series B'];
-	  $scope.data = [
-	    [65, 59, 80, 81, 56, 55, 40],
-	    [28, 48, 40, 19, 86, 27, 90]
-	  ];
-	  $scope.onClick = function (points, evt) {
-	    console.log(points, evt);
-	  };
-	  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+	  $scope.labels = ["1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s"];
+	  $scope.series = ['Core #0', 'Core #1', 'Core #2', 'Core #3'];
+	  
+	  $scope.cpu0check = true;
+	  $scope.cpu1check = true;
+	  $scope.cpu2check = true;
+	  $scope.cpu3check = true;
+
+	  
+	  $scope.core0 = [88, 45, 41, 79, 26, 57, 30, 65, 59, 80];
+	  $scope.core1 = [65, 59, 80, 81, 56, 55, 40, 79, 26, 57];
+	  $scope.core2 = [28, 48, 40, 19, 86, 27, 90, 80, 81, 56];
+	  $scope.core3 = [56, 23, 37, 21, 96, 25, 50, 28, 48, 40];
+	  
+	  $scope.data = [$scope.core0, $scope.core1, $scope.core2, $scope.core3];
+
+	  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
 	  $scope.options = {
 	    scales: {
 	      yAxes: [
@@ -89,24 +96,63 @@
 	          type: 'linear',
 	          display: true,
 	          position: 'left'
-	        },
+	        }
+	      ]
+	    }
+	  };
+	  
+	  this.updateData = function(){
+		  console.log("working?");
+		  $scope.data = [];
+		  if($scope.cpu0check)
+		  	$scope.data.push($scope.core0);
+		  if($scope.cpu1check)
+		  	$scope.data.push($scope.core1);
+		  if($scope.cpu2check)
+		  	$scope.data.push($scope.core2);
+		  if($scope.cpu3check)
+		  	$scope.data.push($scope.core3);
+	  }
+	});
+	
+	app.controller("NetworkCtrl", function ($scope) {
+
+	  $scope.labels = ["1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s"];
+	  $scope.series = ['Upstream','Downstream'];
+
+	  $scope.data = [ [56, 23, 37, 21, 96, 25, 50, 28, 48, 40]];
+
+	  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+	  $scope.options = {
+	    scales: {
+	      yAxes: [
 	        {
-	          id: 'y-axis-2',
+	          id: 'y-axis-1',
 	          type: 'linear',
 	          display: true,
-	          position: 'right'
+	          position: 'left'
 	        }
 	      ]
 	    }
 	  };
 	});
 	
-	app.controller('DoughnutCtrl', ['$scope', function ($scope) {
+	app.controller('StorageCtrl', ['$scope', function ($scope) {
 	    $scope.labels = ['Used', 'Free'];
 	    $scope.data = [300, 500];
 	    
 
 	}]);
+	
+	app.controller("RamCtrl", function ($scope) {
+	  $scope.labels = ["Unity", "Chrome", "Edge"];
+	  $scope.data = [300, 500, 100];
+	});
+	
+	app.controller('DashController', ['$scope', function($scope){
+		$scope.screen = 0;
+	}]);
+              
 
  })();
 
