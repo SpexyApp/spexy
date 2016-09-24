b92c8c3448dd7c17eae9082ab2aa4a7db48d1ec8
nisargkolhe <nisargkolhe@gmail.com>
2016-09-24 12:04:33 -0400

    changed data from json to form

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -23,10 +23,12 @@
 			username: "",
 			password: "",
 		};
+		
+		var subData = $.param({username: $scope.auth.username, password: $scope.auth.password});
 
 		this.submitForm = function(){
 			$scope.status = "Submitting...";
-			$http.post('/auth', $scope.auth).
+			$http.post('/auth', subData).
                 success(function(data, status) {
                     if(data.success === true){
 					    $scope.status = "Submit";
